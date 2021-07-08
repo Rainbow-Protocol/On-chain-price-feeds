4893fd5bcd4138ea257de7fae2bb58a5a312bc96
oxRainbow <>
2021-07-08 17:12:44 +0800

    Uniform interface name.

--- contracts/OraclePriceFeedsInterface.sol
+++ contracts/OraclePriceFeedsInterface.sol
@@ -1,6 +1,6 @@
 pragma solidity >=0.8.0;
 
-interface OpenOraclePriceData {
+interface OraclePriceFeeds {
     function getPriceTokenToUsdt(address srcTokenAddress) external view returns(uint256 isValid, int256 lastPrice);
 }
 
